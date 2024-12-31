897998dac07c324e89d6fa99611eb32affc69de7
Asiffg <Asiffgei1999@gmail.com>
2024-12-31 13:25:29 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+console.log("Here is the table of number you entered")
+let num =prompt("Enter the number for the table")
+for (let i = 1; i<= 10 ; i++){
+    console.log(`${num} x ${i} = ${num*i}`)
+}
